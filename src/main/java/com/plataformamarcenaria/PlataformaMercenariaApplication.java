d0292b31749b5e1c43fd008dbe28ef22e5dfdff5
Juan Fonseca <juanof2000@gmail.com>
2025-08-15 23:12:50 -0300

    create new implementations and configure the api

--- src/main/java/com/plataformamarcenaria/PlataformaMercenariaApplication.java
+++ src/main/java/com/plataformamarcenaria/PlataformaMercenariaApplication.java
@@ -2,10 +2,12 @@ package com.plataformamarcenaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan; // Import necessário para a anotação
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.plataformamarcenaria") // Adicionando a anotação que vai forçar o scan
 public class PlataformaMercenariaApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlataformaMercenariaApplication.class, args);
     }
-} 
+}
---
+++ src/main/java/com/plataformamarcenaria/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.plataformamarcenaria.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // Desabilita o CSRF, útil para APIs REST
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+}
